9d2d6faa6b702338e37bc1ba3866aa54b928de07
rifim01 <rifim01@adminrifim.org>
2026-06-01 23:01:06 +0700

    Create service-worker.js

---
+++ rifim-erp/service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install',e=>{
+
+  e.waitUntil(
+
+    caches.open('rifim-cache')
+    .then(cache=>{
+
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js'
+      ]);
+
+    })
+
+  );
+
+});
